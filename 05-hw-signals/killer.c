83ae3c1ff21c22f5428480a6d980878799696bb8
Tanner Clements <tc424@arkansas.cs.byu.edu>
2024-02-09 17:13:07 -0700

    hw 5

--- 05-hw-signals/killer.c
+++ 05-hw-signals/killer.c
@@ -28,24 +28,101 @@ int main(int argc, char *argv[]) {
 
 	switch (scenario[0]) {
 	case '0':
+		kill(pid, SIGHUP);
+		sleep(1);
 		break;
 	case '1':
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '2':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '3':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 31);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(4);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '4':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGINT);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '5':
+		kill(pid, SIGHUP);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '6':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 10);
+		sleep(4);
+		kill(pid, 16);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '7':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 10);
+		sleep(4);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '8':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 31);
+		sleep(1);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, 30);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
-	case '9': 
+	case '9':
+		kill(pid, 31);
+		sleep(4);
+		kill(pid, SIGQUIT);
+		sleep(1);
+		kill(pid, 31);
+		sleep(4);
+		kill(pid, 12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 
 	}
